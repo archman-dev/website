7f73fd008646c4593c9168aa750dad200f251fc3
Dmitri Meshin <dmitri.meshin@gmail.com>
2025-09-04 19:06:07 +0300

    fix(config): disable contextual search in Algolia configuration

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -202,7 +202,7 @@ const config: Config = {
       indexName: "archman_dev_lp3n4a03rl_pages",
 
       // Optional: see doc section below
-      contextualSearch: true,
+      contextualSearch: false,
 
       // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
       // externalUrlRegex: 'external\\.com|domain\\.com',
